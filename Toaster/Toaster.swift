ea5a1fa743805a6d0293c00e00cb16221c074e7f
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2022-01-09 21:48:49 +0700

    Обновление

--- Toaster/Toaster.swift
+++ Toaster/Toaster.swift
@@ -11,111 +11,87 @@ import UIKit
 private let offset: CGFloat = 3
 
 private class Toaster : UIView {
-    
-    fileprivate override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.removeFromSuperview()
     }
     
     var duration: TimeInterval = 2
     
     override func willMove(toSuperview newSuperview: UIView?) {
-        
         super.willMove(toSuperview: newSuperview)
         
-        if let _ = newSuperview {
-            
-            let delayTime = DispatchTime.now() + Double(Int64(self.duration * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-            
-            DispatchQueue.main.asyncAfter(deadline: delayTime) { [weak self] () in
-                
+        if newSuperview != nil {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.duration) { [weak self] in
                 self?.hideToaster()
             }
         }
     }
     
     convenience init(topLeftCorner: CGPoint, message: String) {
-        
         let screenBounds = UIScreen.main.bounds.size
-        
         let estimatedSize = CGSize(width: screenBounds.width/3, height: screenBounds.height/3)
         
-        let label = Toaster.label(message, size: estimatedSize)
+        let label = Self.label(message, size: estimatedSize)
         label.frame.origin = CGPoint(x: offset, y: offset)
         
         self.init(frame: CGRect(origin: topLeftCorner, size: label.frame.insetBy(dx: -offset, dy: -offset).size))
-        
         self.addSubview(label)
     }
     
     convenience init(center: CGPoint, message: String) {
-        
         let screenBounds = UIScreen.main.bounds.size
-        
         let estimatedSize = CGSize(width: screenBounds.width/3, height: screenBounds.height/3)
         
-        let label = Toaster.label(message, size: estimatedSize)
+        let label = Self.label(message, size: estimatedSize)
         label.frame.origin = CGPoint(x: offset, y: offset)
         
         let origin = CGPoint(x: center.x - label.frame.width/2, y: center.y - label.frame.height/2)
         
         self.init(frame: CGRect(origin: origin, size: label.frame.insetBy(dx: -offset, dy: -offset).size))
-        
         self.addSubview(label)
     }
     
     convenience init(topLeftCorner: CGPoint, attributedMessage: NSAttributedString) {
-        
         let screenBounds = UIScreen.main.bounds.size
-        
         let estimatedSize = CGSize(width: screenBounds.width/3, height: screenBounds.height/3)
         
-        let label = Toaster.label(attributedMessage, size: estimatedSize)
+        let label = Self.label(attributedMessage, size: estimatedSize)
         label.frame.origin = CGPoint(x: offset, y: offset)
         
         self.init(frame: CGRect(origin: topLeftCorner, size: label.frame.insetBy(dx: -offset, dy: -offset).size))
-        
         self.addSubview(label)
     }
     
     convenience init(center: CGPoint, attributedMessage: NSAttributedString) {
-        
         let screenBounds = UIScreen.main.bounds.size
-        
         let estimatedSize = CGSize(width: screenBounds.width/3, height: screenBounds.height/3)
         
-        let label = Toaster.label(attributedMessage, size: estimatedSize)
+        let label = Self.label(attributedMessage, size: estimatedSize)
         label.frame.origin = CGPoint(x: offset, y: offset)
         
         let origin = CGPoint(x: center.x - label.frame.width/2, y: center.y - label.frame.height/2)
         
         self.init(frame: CGRect(origin: origin, size: label.frame.insetBy(dx: -offset, dy: -offset).size))
-        
         self.addSubview(label)
     }
     
-    override fileprivate init(frame: CGRect) {
-        
+    override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.autoresizingMask = ([.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin])
-        
         self.layer.cornerRadius = 10.0
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOpacity = 0.6
         self.layer.shadowRadius = 4.0
         self.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
-        
         self.backgroundColor = UIColor.black.withAlphaComponent(0.8)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        
         fatalError("init(coder:) has not been implemented")
     }
     
-    fileprivate class func label(_ message: String, size: CGSize) -> UILabel {
-        
+    static func label(_ message: String, size: CGSize) -> UILabel {
         let label = UILabel(frame: CGRect(origin: CGPoint.zero, size: size))
         label.numberOfLines = 0
         label.font = UIFont.boldSystemFont(ofSize: 14.0)
@@ -129,8 +105,7 @@ private class Toaster : UIView {
         return label
     }
     
-    fileprivate class func label(_ message: NSAttributedString, size: CGSize) -> UILabel {
-        
+    class func label(_ message: NSAttributedString, size: CGSize) -> UILabel {
         let label = UILabel(frame: CGRect(origin: CGPoint.zero, size: size))
         label.numberOfLines = 0
         label.textAlignment = NSTextAlignment.center
@@ -142,18 +117,14 @@ private class Toaster : UIView {
         return label
     }
     
-    fileprivate func hideToaster() {
-        
+    func hideToaster() {
         let defaultDuration: TimeInterval = 0.7
         
-        UIView.animate(withDuration: defaultDuration, animations: { [weak self] () -> Void in
-            
+        UIView.animate(withDuration: defaultDuration, animations: { [weak self] in
             self?.alpha = 0
             
-        }, completion: { [weak self] (finished) -> Void in
-            
+        }, completion: { [weak self] (finished) in
             if let strongSelf = self {
-                
                 strongSelf.removeFromSuperview()
             }
         }) 
@@ -161,9 +132,7 @@ private class Toaster : UIView {
 }
 
 public extension UIView {
-    
     func showToaster(withMessage message: String, center: CGPoint, duration: TimeInterval = 2.0) {
-        
         let toaster = Toaster(center: center, message: message)
         toaster.duration = duration
         
@@ -171,7 +140,6 @@ public extension UIView {
     }
     
     func showToaster(withMessage message: String, topLeftCorner: CGPoint, duration: TimeInterval = 2.0) {
-        
         let toaster = Toaster(topLeftCorner: topLeftCorner, message: message)
         toaster.duration = duration
         
@@ -179,7 +147,6 @@ public extension UIView {
     }
     
     func showToaster(withAttributedMessage attributedMessage: NSAttributedString, topLeftCorner: CGPoint, duration: TimeInterval = 2.0) {
-        
         let toaster = Toaster(topLeftCorner: topLeftCorner, attributedMessage: attributedMessage)
         toaster.duration = duration
         
@@ -187,7 +154,6 @@ public extension UIView {
     }
     
     func showToaster(withAttributedMessage attributedMessage: NSAttributedString, center: CGPoint, duration: TimeInterval = 2.0) {
-        
         let toaster = Toaster(center: center, attributedMessage: attributedMessage)
         toaster.duration = duration
         
--- Toaster/ViewController.swift
+++ Toaster/ViewController.swift
@@ -9,30 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func toastAtCustomTopLeftCorner() {
+        self.view.backgroundColor = .white
         
+        let toastAtCustomTopLeftCornerButton = UIButton(type: .system)
+        toastAtCustomTopLeftCornerButton.setTitle("Toast at custom top left corner", for: .normal)
+        toastAtCustomTopLeftCornerButton.addTarget(self, action: #selector(self.toastAtCustomTopLeftCorner(_:)), for: .touchUpInside)
+        toastAtCustomTopLeftCornerButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(toastAtCustomTopLeftCornerButton)
+        toastAtCustomTopLeftCornerButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        toastAtCustomTopLeftCornerButton.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        toastAtCustomTopLeftCornerButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        
+        let toastAtCustomCenterButton = UIButton(type: .system)
+        toastAtCustomCenterButton.setTitle("Toast at custom center", for: .normal)
+        toastAtCustomCenterButton.addTarget(self, action: #selector(self.toastAtCustomCenter(_:)), for: .touchUpInside)
+        toastAtCustomCenterButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(toastAtCustomCenterButton)
+        toastAtCustomCenterButton.topAnchor.constraint(equalTo: toastAtCustomTopLeftCornerButton.bottomAnchor, constant: 16).isActive = true
+        toastAtCustomCenterButton.leftAnchor.constraint(equalTo: toastAtCustomTopLeftCornerButton.leftAnchor).isActive = true
+        toastAtCustomCenterButton.rightAnchor.constraint(equalTo: toastAtCustomTopLeftCornerButton.rightAnchor).isActive = true
+        
+        let toastWithAttributedStringButton = UIButton(type: .system)
+        toastWithAttributedStringButton.setTitle("Toast with attributed string", for: .normal)
+        toastWithAttributedStringButton.addTarget(self, action: #selector(self.toastAtCustomCenter(_:)), for: .touchUpInside)
+        toastWithAttributedStringButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(toastWithAttributedStringButton)
+        toastWithAttributedStringButton.topAnchor.constraint(equalTo: toastAtCustomCenterButton.bottomAnchor, constant: 16).isActive = true
+        toastWithAttributedStringButton.leftAnchor.constraint(equalTo: toastAtCustomCenterButton.leftAnchor).isActive = true
+        toastWithAttributedStringButton.rightAnchor.constraint(equalTo: toastAtCustomCenterButton.rightAnchor).isActive = true
+    }
+
+    @objc func toastAtCustomTopLeftCorner(_ sender: UIButton) {
         self.view.showToaster(withMessage: "Tap to hide annoying view.", topLeftCorner: CGPoint(x: 200, y: 200))
     }
     
-    @IBAction func toastAtCustomCenter() {
-        
+    @objc func toastAtCustomCenter(_ sender: UIButton) {
         self.view.showToaster(withMessage: "Tap to hide annoying view.", center: CGPoint(x: 200, y: 200))
     }
     
-    @IBAction func toastWithAttributedString() {
-        
+    @objc func toastWithAttributedString(_ sender: UIButton) {
         let attributedString = NSAttributedString(string: "Tap to hide annoying view.", attributes: [.backgroundColor: UIColor.blue, .font: UIFont(name: "Helvetica", size: 20)!])
         
         self.view.showToaster(withAttributedMessage: attributedString, topLeftCorner: CGPoint(x: 200, y: 200))
